6a1b6e0bf2dc575589ae2854c2cfe9702a6d8fee
Aaron Critchley <aaron.critchley@gmail.com>
2019-01-06 02:55:16 +0000

    Adding a vectorized approach

--- performance/2017_12_LU_decomposition/python/profile_and_plot.py
+++ performance/2017_12_LU_decomposition/python/profile_and_plot.py
@@ -14,18 +14,18 @@ from lu_smorgasbord import (
     lu_parallel, lu_parallel_2
 )
 from lu_c_fortran import lu_decomp_c_fortran
-
+from lu_vectorized import lu_vectorized, lu_vectorized_experimental
 
 if __name__ == '__main__':
     np.random.seed(0)
 
     lu_decomp_python = lu_decomp
 
-    def collect_results(fn, jit=True):
-        if jit:
+    def collect_results(fn, fast=True, should_jit=True):
+        if fast:
             sizes = (100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 2000)
 
-            if not isinstance(fn, numba.targets.registry.CPUDispatcher):
+            if should_jit and not isinstance(fn, numba.targets.registry.CPUDispatcher):
                 fn = njit(fn)
 
             fn(np.random.random((10, 10)))  # force compile
@@ -49,7 +49,7 @@ if __name__ == '__main__':
         raw_df = pd.DataFrame(raw_results, columns=['idx', 'vals']).set_index('idx')
         return raw_df
 
-    df = collect_results(lu_decomp_python, jit=False)
+    df = collect_results(lu_decomp_python, fast=False, should_jit=False)
     plt.loglog(df, label=lu_decomp_python.__name__)
 
     # jitted functions
@@ -57,8 +57,12 @@ if __name__ == '__main__':
            lu_3, lu_4, lu_5, lu_parallel, lu_parallel_2]
 
     for fn in fns:
-        df = collect_results(fn)
-        plt.loglog(df, label=fn.__name__)
+        plt.loglog(collect_results(fn), label=fn.__name__)
+
+    # We don't want to jit our vectorized functions, as calling guvectorized
+    # functions from a jitted function is troublesome and not currently well supported
+    for fn in [lu_vectorized, lu_vectorized_experimental]:
+        plt.loglog(collect_results(fn, should_jit=False), label=fn.__name__)
 
     plt.xlabel('Length of array axis (n)')
     plt.ylabel('Calculation time (s)')
